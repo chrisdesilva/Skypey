9687d827f027ccea4f813599f931d35e761e0438
unknown <chris.a.desilva@gmail.com>
2019-04-02 19:23:35 -0500

    Update main to reflect activeuserid status

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,12 +6,12 @@ import _ from 'lodash';
 import './App.css';
 
 const App = () => {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId } = store.getState();
   
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)}/>
-      <Main />
+      <Main user={user} activeUserId={activeUserId} />
     </div>
   );
 }
