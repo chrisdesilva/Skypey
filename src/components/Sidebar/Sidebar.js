40cec3e772b3f4a5a3598e9f8c86820e8e3ec2b5
unknown <chris.a.desilva@gmail.com>
2019-04-05 21:22:14 -0500

    Add ability to change main display based on user active state

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import './Sidebar.css';
 const Sidebar = ({ contacts }) => {
   return ( 
     <aside className="Sidebar">
-      {contacts.map(contact => <User user={contact} key={contact.user_id} />)}
+      {contacts.map( (contact, index) => <User key={contact}  user={contact} />)}
     </aside>
   );
 };
